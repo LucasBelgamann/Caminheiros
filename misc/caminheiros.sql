f80257aade13df20962f31363ee617901158da31
LucasBelgamann <lucasbelgamann@hotmail.com>
2023-07-03 23:41:19 -0300

    Feat: Coluna Created_at e updated_at

--- misc/caminheiros.sql
+++ misc/caminheiros.sql
@@ -7,21 +7,27 @@ CREATE TABLE caminheirosdb.Users (
   phone TEXT NOT NULL,
   email TEXT NOT NULL,
   password TEXT NOT NULL,
-  role TEXT NOT NULL
+  role TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE caminheirosdb.Groups (
   id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
   name TEXT NOT NULL,
   userId INTEGER,
-  FOREIGN KEY (userId) REFERENCES caminheirosdb.Users (id)
+  FOREIGN KEY (userId) REFERENCES caminheirosdb.Users (id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE caminheirosdb.Meetings (
   id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
   date TEXT NOT NULL,
   groupId INTEGER,
-  FOREIGN KEY (groupId) REFERENCES caminheirosdb.Groups (id)
+  FOREIGN KEY (groupId) REFERENCES caminheirosdb.Groups (id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE caminheirosdb.Groups_has_users (
@@ -38,9 +44,9 @@ CREATE TABLE caminheirosdb.Meetings_has_users (
   FOREIGN KEY (userId) REFERENCES caminheirosdb.Users (id)
 );
 
-
-INSERT INTO caminheirosdb.Users (name, phone, email, password, role) VALUES
-('Lucas Gabriel Agostinho Belgamann', '(41) 98830-9378', 'lucasbelgamann@hotmail.com', 'lucaspass', 'admin'),
-('Usuário 2', '987654321', 'usuario2@example.com', 'senha2', 'user'),
-('Usuário 3', '987654321', 'usuario3@example.com', 'senha3', 'user'),
-('Usuário 4', '987654321', 'usuario4@example.com', 'senha4', 'user');
+INSERT INTO caminheirosdb.Users (name, phone, email, password, role, created_at, updated_at)
+VALUES
+('Lucas Gabriel Agostinho Belgamann', '(41) 98830-9378', 'lucasbelgamann@hotmail.com', 'lucaspass', 'admin', CONVERT_TZ(NOW(), 'UTC', 'America/Sao_Paulo'), CONVERT_TZ(NOW(), 'UTC', 'America/Sao_Paulo')),
+('Usuário 2', '987654321', 'usuario2@example.com', 'senha2', 'user', CONVERT_TZ(NOW(), 'UTC', 'America/Sao_Paulo'), CONVERT_TZ(NOW(), 'UTC', 'America/Sao_Paulo')),
+('Usuário 3', '987654321', 'usuario3@example.com', 'senha3', 'user', CONVERT_TZ(NOW(), 'UTC', 'America/Sao_Paulo'), CONVERT_TZ(NOW(), 'UTC', 'America/Sao_Paulo')),
+('Usuário 4', '987654321', 'usuario4@example.com', 'senha4', 'user', CONVERT_TZ(NOW(), 'UTC', 'America/Sao_Paulo'), CONVERT_TZ(NOW(), 'UTC', 'America/Sao_Paulo'));
